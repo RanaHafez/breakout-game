aafd8197e0072151cedb22947ab02026166ebaa1
RanaHafez <ra2017ha1999@gmail.com>
2022-05-21 15:21:10 -0700

    adding rows with different colors

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-######################### Pong Game #############################
-import turtle
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
@@ -7,20 +5,8 @@ import time
 from scoreboard import ScoreBoard
 from block import Block
 
-
-
-COLORS = [
-    "#7D1E6A","#7D1E6A",
-    "#EEB0B0", "#EEB0B0",
-    "#FFE59D", "#FFE59D",
-    "#BDE6F1", "#BDE6F1",
-]
-SCORES = [
-    1,1,
-    3,3,
-    5,5,
-    7,7
-]
+COLORS = ["#7D1E6A", "#EEB0B0", "#FFE59D", "#BDE6F1"]
+SCORES = [1, 3, 5, 7]
 screen = Screen()
 paddle = Paddle()
 ball = Ball()
@@ -29,20 +15,22 @@ scoreboard = ScoreBoard()
 bricks = []
 x = -360
 y = -50
-for i in range(8):
-    while x < 380:
-        block = Block(color=COLORS[i], position=(x, y), assigned_score=SCORES[i])
-        bricks.append(block)
-        x += 50
-    x = -360
-    y += 30
+for i in range(4):
+    for j in range(2):
+        # creating 2 rows of the same color and value
+        while x < 380:
+            block = Block(color=COLORS[i], position=(x, y), assigned_score=SCORES[i])
+            bricks.append(block)
+            x += 50
+        x = -360
+        y += 30
 
 
 screen.bgcolor("#FF5D5D")
 screen.setup(width=800, height=600)
 screen.title(titlestring="Breakout Game")
 # Stopping the animation does not work ??
-screen.tracer(0,0)
+screen.tracer(n=0,delay=0)
 screen.listen()
 screen.onkey(key="Right", fun=paddle.move_right)
 screen.onkey(key="Left", fun=paddle.move_left)
@@ -76,6 +64,7 @@ while game_on:
             bricks.remove(brick)
             break
 
+    # Detect collision with the bottom wall.
     if ball.ycor() < -290:
         ball.reset_ball()
         scoreboard.update_tries()
